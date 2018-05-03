0c80bc3ee1d36b4ca633450ad0315677a0916084
Sebastian Krezel <s.krezel@excellbit.pl>
2018-05-03 21:22:07 +0200

    exercise 13.2 secon update

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/taskList/dao/TaskListDao.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/taskList/dao/TaskListDao.java
@@ -1,4 +1,15 @@
 package com.kodilla.hibernate.taskList.dao;
 
-public class TaskListDao {
+import com.kodilla.hibernate.taskList.TaskList;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+@Transactional
+public interface TaskListDao extends CrudRepository<TaskList, Integer> {
+    List<TaskList> findByListName(String listName);
+
 }
--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/TaskListDao/TaskListDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/TaskListDao/TaskListDaoTestSuite.java
@@ -1,4 +1,37 @@
 package com.kodilla.hibernate.TaskListDao;
 
+import com.kodilla.hibernate.taskList.TaskList;
+import com.kodilla.hibernate.taskList.dao.TaskListDao;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class TaskListDaoTestSuite {
+    @Autowired
+    private TaskListDao taskListDao;
+
+    @Test
+    public void testFindByListName(){
+
+        //Given
+        TaskList taskList = new TaskList("learningJava","An attempt to learn programming in Java using the Kodilla course");
+
+        //When
+        taskListDao.save(taskList);
+
+        //Then
+        List<TaskList> taskListNames = taskListDao.findByListName("learningJava");
+        String searchTaskName = taskListNames.get(0).getListName();
+        Assert.assertEquals(searchTaskName, "learningJava");
+
+        //CleanUp
+        taskListDao.delete(taskList.getId());
+    }
 }
