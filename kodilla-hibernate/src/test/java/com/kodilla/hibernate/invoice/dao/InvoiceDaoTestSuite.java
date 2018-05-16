d53c11c1e7353285c1a542d6d165d9473bd1879e
Sebastian Krezel <s.krezel@excellbit.pl>
2018-05-16 21:59:22 +0200

    Exercice 13.3 ver 4

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -3,6 +3,7 @@ package com.kodilla.hibernate.invoice.dao;
 import com.kodilla.hibernate.invoice.Invoice;
 import com.kodilla.hibernate.invoice.Item;
 import com.kodilla.hibernate.invoice.Product;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,29 +27,45 @@ public class InvoiceDaoTestSuite {
     @Test
     public void testInvoiceDaoSave() {
         //Given
-        Product product01 = new Product("Cheese");
-        Product product02 = new Product("Butter");
-        Product product03 = new Product("Bread");
-        Product product04 = new Product("Eggs");
+        Product cheese = new Product("Cheese");
+        Product butter = new Product("Butter");
+        Product bread = new Product("Bread");
+        Product eggs = new Product("Eggs");
 
-        Item item01 = new Item(product01, new BigDecimal(30), 5);
-        Item item02 = new Item(product02, new BigDecimal(120), 2);
-        Item item03 = new Item(product03, new BigDecimal(8), 3);
-        Item item04 = new Item(product04, new BigDecimal(1), 5);
-        Item item05 = new Item(product02, new BigDecimal(5), 3);
+        Item item01 = new Item(cheese, new BigDecimal(30), 5);
+        Item item02 = new Item(butter, new BigDecimal(120), 2);
+        Item item03 = new Item(bread, new BigDecimal(8), 3);
+        Item item04 = new Item(eggs, new BigDecimal(1), 5);
+        Item item05 = new Item(eggs, new BigDecimal(5), 3);
+
+        List<Item> itemList = new ArrayList<>();
+        itemList.add(item01);
+        itemList.add(item02);
+        itemList.add(item03);
+        itemList.add(item04);
+        itemList.add(item05);
+
+        cheese.setItems(itemList);
+        butter.setItems(itemList);
+        bread.setItems(itemList);
+        eggs.setItems(itemList);
 
         Invoice invoice = new Invoice("FV 07/05/2018");
 
-        invoice.getItems().add(item01);
-//        invoice1.getItems().add(item02);
-//        invoice1.getItems().add(item03);
+        item01.setInvoice(invoice);
+        item02.setInvoice(invoice);
+        item03.setInvoice(invoice);
+        item04.setInvoice(invoice);
+        item05.setInvoice(invoice);
+
 
         //When
         invoiceDao.save(invoice);
 
+        int invoiceID = invoice.getId();
 
         //Then
-        //Do nothing
+        Assert.assertNotEquals(0,invoiceID);
 
         //Clean UP
 //        try {
